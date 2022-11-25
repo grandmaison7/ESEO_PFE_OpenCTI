86213846d4ae58b3bd1d78b71476d956e9084fba
Gautier Barth <62548038+gbarth-SNproject@users.noreply.github.com>
2022-11-25 10:39:27 +0100

    Correctif install_docker.sh
    Permet de faire tourner le script sur les machines de l'école

--- V1_Swarm_OpenCTI/scripts/install_docker.sh
+++ V1_Swarm_OpenCTI/scripts/install_docker.sh
@@ -1,12 +1,19 @@
 apt update
 
-sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install apt-transport-https
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-get install ca-certificates
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+sudo apt-get install curl
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
+sudo apt-get install gnupg-agent
+
+sudo apt-get install software-properties-common
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose -y
