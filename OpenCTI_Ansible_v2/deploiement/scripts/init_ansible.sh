5e86f6174a9291c134e86b06c5f927b2cc147e3b
Gautier Barth <62548038+gbarth-SNproject@users.noreply.github.com>
2023-01-15 15:01:10 +0100

    Update init_ansible.sh

--- OpenCTI_Ansible_v2/deploiement/scripts/init_ansible.sh
+++ OpenCTI_Ansible_v2/deploiement/scripts/init_ansible.sh
@@ -1,5 +1,5 @@
 sudo useradd -m -p Ans1bLe -s /bin/bash user-ansible
 
-sudo apt install ansible-galaxy collection install community.docker
+sudo ansible-galaxy collection install community.docker
 
 # A FINIR
